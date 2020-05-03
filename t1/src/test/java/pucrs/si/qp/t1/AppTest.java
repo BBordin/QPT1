2de697b47fc28a95d91fbbc630bd27ca8bc2a454
15201048 <15201048@VWEPO01HRZ125.portoalegre.pucrsnet.br>
2020-05-03 12:03:13 -0300

    Update 1

---
+++ t1/src/test/java/pucrs/si/qp/t1/AppTest.java
@@ -0,0 +1,15 @@
+package pucrs.si.qp.t1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+   
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
